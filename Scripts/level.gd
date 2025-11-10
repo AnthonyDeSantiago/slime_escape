0a22fd29a5d7fa231e49a7debf7f4b3cc1fdb1bf
Anthony <anthonyde16@gmail.com>
2025-11-09 21:07:19 -0500

    scraped single click; more platforming

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -10,69 +10,65 @@ extends Node2D
 @export var speed_up_rate = 5
 
 const CAM_SPEED = 0
-var new_position
-var current_norm
-var norm
-var rate = 1
-var epsilon = 0.25
-var is_clockwise = true
-var is_on_wall = false
-var is_on_floor = false
-var is_in_air = false
+var cast_target_position
 
 func _draw():
-	if new_position:
-		draw_dashed_line(player.global_position, new_position, Color.WHEAT, 10, 20, true, true)
-		draw_circle(new_position, 25, Color.GREEN)
+	if cast_target_position:
+		draw_circle(cast_target_position, 10, Color.AQUA)
+		#draw_dashed_line(player.global_position, new_position, Color.WHEAT, 10, 20, true, true)
+		#draw_circle(new_position, 25, Color.GREEN)
 		#if norm:
 			#draw_line(cast.get_collision_point(), norm + new_position,  Color.RED, 10)
 
 func _ready():
-	cast.global_position = player.global_position
-	rate = start_rate
-	cast.exclude_parent = true
-	cast.target_position = cast.target_position * ray_length
-	norm = cast.target_position
+	pass
+	#cast.global_position = player.global_position
+	#rate = start_rate
+	#cast.exclude_parent = true
+	#cast.target_position = cast.target_position * ray_length
+	#norm = cast.target_position
 
 func _process(delta):
-	cast.global_position = player.global_position
-	cast.target_position = cast.target_position.rotated(rate * delta)
-	if current_norm:
-		var approaching_max = abs(cast.target_position.angle_to(current_norm.rotated(PI/2 - epsilon)))
-		var approaching_min = abs(cast.target_position.angle_to(current_norm.rotated(-PI/2 + epsilon)))
-		
-		if approaching_max < epsilon:
-			rotate_counter_clockwise()
-			
-		if approaching_min < epsilon:
-			rotate_clockwise()
-			
-	
-	path_to_follow.progress_ratio += CAM_SPEED * delta
-	
-	if cast.is_colliding():
-		new_position = cast.get_collision_point()
-		norm = (cast.get_collision_normal() * ray_length)
-	else:
-		new_position = null
-		norm = null
-	
-	if cast.is_colliding() and Input.is_action_just_pressed("jump") and new_position:
-		#player.global_position = new_position + cast.get_collision_normal()
-		player.global_position = new_position
-		cast.global_position = player.global_position
-		if player.normal_vector:
-			print("has normal")
-		cast.target_position = norm
-		current_norm = norm
-	
-	if player.is_on_floor():
-		current_norm = Vector2.UP
-	
-	queue_redraw()
+	pass
+	#cast.global_position = player.global_position
+	#cast.target_position = cast.target_position.rotated(rate * delta)
+	#if current_norm:
+		#var approaching_max = abs(cast.target_position.angle_to(current_norm.rotated(PI/2 - epsilon)))
+		#var approaching_min = abs(cast.target_position.angle_to(current_norm.rotated(-PI/2 + epsilon)))
+		#
+		#if approaching_max < epsilon:
+			#rotate_counter_clockwise()
+			#
+		#if approaching_min < epsilon:
+			#rotate_clockwise()
+			#
+	#
+	#path_to_follow.progress_ratio += CAM_SPEED * delta
+	#
+	#if cast.is_colliding():
+		#new_position = cast.get_collision_point()
+		#norm = (cast.get_collision_normal() * ray_length)
+	#else:
+		#new_position = null
+		#norm = null
+	#
+	#if cast.is_colliding() and Input.is_action_just_pressed("jump") and new_position:
+		##player.global_position = new_position + cast.get_collision_normal()
+		#player.global_position = new_position
+		#cast.global_position = player.global_position
+		#if player.normal_vector:
+			#print("has normal")
+		#cast.target_position = norm
+		#current_norm = norm
+	#
+	#if player.is_on_floor():
+		#current_norm = Vector2.UP
+	#
+	#queue_redraw()
 
-func rotate_clockwise():
-	rate = abs(rate)
-
-func rotate_counter_clockwise():
-	rate = -rate
+#func rotate_clockwise():
+	#rate = abs(rate)
+#
+#func rotate_counter_clockwise():
+	#rate = -rate
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,10 +4,13 @@ class_name Player
 @onready var collider: CollisionShape2D = $CollisionShape2D
 @onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+@onready var cast: RayCast2D = $RayCast2D
 
-@export var wall_slide_speed: float = 100
+@export var wall_slide_speed: float = 9000
+@export var teleport_range: float = 800
+@export var SPEED: float = 25000
+@export var JUMP_SPEED: float = -500
 
-var friction = 1000
 var viewport_hieght
 var viewport_width
 var viewport_position
@@ -21,11 +24,10 @@ func _ready() -> void:
 	#print("hieght:", viewport_hieght, " width:", viewport_width, " position:", viewport_position)
 	normal_vector = Vector2.UP
 
-func _draw() -> void:
-	if normal_vector:
-		draw_line(Vector2(0, 0), (normal_vector) * 100, Color.RED, 10, false)
-
 func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("left", "right")
+	shoot_ray()
+		
 	viewport_position = get_viewport_rect().position
 	if not is_on_floor() and not is_on_wall():
 		velocity += get_gravity() * delta
@@ -33,9 +35,36 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor() and not is_on_wall():
 		normal_vector = Vector2(0, 0)
 		pass
+	
+	if cast.is_colliding() and Input.is_action_just_pressed("teleport"):
+		teleport()
+	if not is_on_wall() and not is_on_floor():
+		if direction:
+			velocity.x = direction * SPEED * delta
+			
+	if is_on_floor_only():
+		if direction:
+			print(direction)
+			if direction < 0:
+				flip_h(false)
+			else:
+				flip_h(true)
+			animationPlayer.play("walk")
+			velocity.x = direction * SPEED * delta
+		else:
+			animationPlayer.play("idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	wall_slide(delta)
-	idle()
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_SPEED
+	
+	if is_on_wall_only():
+		wall_slide(delta)
+		if Input.is_action_just_pressed("jump"):
+			velocity.x = JUMP_SPEED/4 * -get_wall_normal().x
+			velocity.y = JUMP_SPEED
+		
+	#idle()
 	move_and_slide()
 	queue_redraw()
 	
@@ -43,15 +72,34 @@ func idle():
 	if is_on_floor():
 		normal_vector = Vector2.UP
 		animationPlayer.play("idle")
-		pass
+		
 func wall_slide(delta: float):
-	if is_on_wall_only():
-		var dot_prod = get_wall_normal().dot(Vector2.RIGHT)
-		if dot_prod > 0:
-			animatedSprite.flip_h = false
-			normal_vector = Vector2.RIGHT
-		else:
-			animatedSprite.flip_h = true
-			normal_vector = Vector2.LEFT
-		animationPlayer.play("wall_slide")
-		velocity.y = friction * delta
+	var dot_prod = get_wall_normal().dot(Vector2.RIGHT)
+	if dot_prod > 0:
+		animatedSprite.flip_h = false
+		normal_vector = Vector2.RIGHT
+	else:
+		animatedSprite.flip_h = true
+		normal_vector = Vector2.LEFT
+	animationPlayer.play("wall_slide")
+	velocity.y = wall_slide_speed * delta
+		
+func shoot_ray():
+	cast.target_position = (get_global_mouse_position() - global_position).normalized() * teleport_range
+	if cast.is_colliding():
+		get_parent().cast_target_position = cast.get_collision_point()
+	else:
+		get_parent().cast_target_position = Vector2(0, 0)
+	get_parent().queue_redraw()
+
+func teleport():
+	if cast.is_colliding():
+		global_position = cast.get_collision_point()
+		velocity = Vector2(0, 0)
+
+func flip_h(flag: bool):
+	if flag:
+		animatedSprite.flip_h = true
+	else:
+		animatedSprite.flip_h = false
+	
